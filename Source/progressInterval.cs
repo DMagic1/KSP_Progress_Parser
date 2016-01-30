a0477b12d85bd9310d9041f5d209cf377dde97d6
DMagic <david.grandy@gmail.com>
2016-01-30 17:36:59 -0500

    Fix interval calculation

--- Source/progressInterval.cs
+++ Source/progressInterval.cs
@@ -1,4 +1,30 @@
-﻿using System;
+﻿#region license
+/*The MIT License (MIT)
+Progress Interval - A storage object for holding all interval-type progress nodes
+
+Copyright (c) 2016 DMagic
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -29,9 +55,9 @@ namespace ProgressParser
 		{
 			pType = t;
 			id = n.Id;
-			currentRecord = r;
 			totalIntervals = i;
 			descriptor = s;
+			currentRecord = r;
 			max = m;
 			round = ro;
 			records = new List<int>(i + 1);
@@ -43,15 +69,17 @@ namespace ProgressParser
 				rewards.Add(new Vector3());
 			}
 
-			interval = getNextInterval();
-
-			if (interval > totalIntervals + 1)
+			if (n.IsComplete)
 				interval = totalIntervals + 1;
+			else
+			{
+				interval = getNextInterval();
 
-			if (interval <= 1)
-				return;
+				if (interval > totalIntervals + 1)
+					interval = totalIntervals + 1;
+			}
 
-			for (int j = 1; j < totalIntervals + 1; j++)
+			for (int j = 1; j <= totalIntervals; j++)
 			{
 				calculateRewards(j);
 			}
@@ -72,15 +100,15 @@ namespace ProgressParser
 			int newInterval = 1;
 			double multiplier = Math.Pow(100.0, 1.0 / (totalIntervals - 1.0));
 
-			while (newInterval <= totalIntervals)
+			for (int i = 1; i <= totalIntervals; i++)
 			{
-				double d = max * (0.01 * Math.Pow(multiplier, (double)(newInterval - 1)));
+				double d = max * (0.01 * Math.Pow(multiplier, (double)(i - 1)));
 				d = Math.Round(d / round) * round;
 
 				if (d > currentRecord)
-					return newInterval;
+					return i;
 
-				newInterval++;
+				newInterval = i;
 			}
 
 			return newInterval;
